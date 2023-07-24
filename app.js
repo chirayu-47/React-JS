4dee3af6cff0ce44fe10bb7ba692cb0b955923aa
chirayu-47 <ck8161@gmail.com>
2023-07-24 14:24:55 +0530

    episode 02

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement(
     "div",
     { id: "child" },
-    React.createElement("h1", {}, "I'm H1 tag")
+    React.createElement("h1", {}, "I'm H1 tag !!")
   )
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
